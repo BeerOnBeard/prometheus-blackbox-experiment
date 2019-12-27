4634fcfdb1a6f649a2d5c63fe72e59480ee06cf8
BeerOnBeard <repoguy156+github@gmail.com>
2019-12-27 12:36:58 -0500

    Deleting generated configs in teardown.

--- teardown.sh
+++ teardown.sh
@@ -4,3 +4,5 @@ set -x;
 docker kill pbe_blackbox pbe_prometheus pbe_alertmanager;
 docker rm pbe_blackbox pbe_prometheus pbe_alertmanager;
 docker network rm pbe_monitoring;
+rm ./alertmanager.yml;
+rm ./prometheus.yml;
